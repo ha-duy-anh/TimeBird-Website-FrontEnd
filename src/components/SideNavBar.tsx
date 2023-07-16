bef64cb26281b11f7380c9913d442fb8050686bc
Ha Duy Anh <haduyanh2310@gmail.com>
2023-07-17 01:13:27 +0700

    3rd

--- src/components/SideNavBar.tsx
+++ src/components/SideNavBar.tsx
@@ -1,18 +1,87 @@
-import React from "react";
-import schoolLogo from '../../assets/uniLogo.svg'
+import React from "react"
+import schoolLogo from '../assets/uniLogo.svg'
+import icDashboard from '../assets/iconDash.svg'
+import icNoti from '../assets/ic_notifications.svg'
+import icSem from '../assets/ic_semester.svg'
+import icCourses from '../assets/ic_courses.svg'
+import icUsers from '../assets/ic_user.svg'
+import icAudit from '../assets/ic_audit.svg'
 
 class SideNavBar extends React.Component {
     render() {
         return (
-            <div className="flex">
-                <div className="w-1/6 h-screen top-0 bg-[#ffffff]">
+            <div>
+                <div className="absolute w-[320px] h-screen top-0 bg-[#ffffff]">
                     <img 
-                        src="schoolLogo"
-                        className="mix-blend-screen"
+                        src={schoolLogo}
+                        className="ml-[68px]"
                         alt="School Logo"
                     />
+
+                    <div className='px-[10px] py-[8px] ml-[16px] mt-[75px]'>
+                        <img 
+                            src={icDashboard}
+                            className='inline-block'
+                            alt='Dashboard Icon'
+                        />
+                        <p className='inline-flex font-[400] text-xl ml-[20px] mt-0 mb-0 text-[#282828]'>Dashboard</p>
+                    </div>
+                    <div className='px-[10px] py-[8px] ml-[16px]'>
+                        <img 
+                            src={icNoti}
+                            className='inline-block'
+                            alt='Notification Icon'
+                        />
+                        <p className='inline-flex font-[400] text-xl ml-[20px] mt-0 mb-0 text-[#282828]'>Notification</p>
+                    </div>
+
+                    <br></br>
+                    <p className='font-bold text-2xl ml-[30px]'>Management</p>
+
+                    <div className='px-[10px] py-[8px] ml-[16px] mt-[20px]'>
+                        <img 
+                            src={icSem}
+                            className='inline-block'
+                            alt='Dashboard Icon'
+                        />
+                        <p className='inline-flex font-[400] text-xl ml-[20px] mt-0 mb-0 text-[#282828]'>Semester</p>
+                    </div>
+
+                    <div className='bg-[#9DBAFF]'>
+                        <div className='px-[10px] py-[8px] ml-[16px]'>
+                            <img 
+                                src={icCourses}
+                                className='inline-block'
+                                alt='Dashboard Icon'
+                            />
+                            <p className='inline-flex font-[400] text-xl ml-[20px] mt-0 mb-0 text-[#0030A5]'>Courses</p>
+                        </div>
+                    </div>
+                    
+                    <div className='px-[10px] py-[8px] ml-[16px]'>
+                        <img 
+                            src={icUsers}
+                            className='inline-block'
+                            alt='Dashboard Icon'
+                        />
+                        <p className='inline-flex font-[400] text-xl ml-[20px] mt-0 mb-0 text-[#282828]'>Users</p>
+                    </div>
+
+                    <br></br>
+                    <p className='font-bold text-2xl ml-[30px]'>Moderation</p>
+
+                    <div className='px-[10px] py-[8px] ml-[16px] mt-[20px]'>
+                        <img 
+                            src={icAudit}
+                            className='inline-block'
+                            alt='Dashboard Icon'
+                        />
+                        <p className='inline-flex font-[400] text-xl ml-[20px] mt-0 mb-0 text-[#282828]'>Audit Log</p>
+                    </div>
                 </div>
             </div>
         );
     }
 }
+
+export default SideNavBar;
--- src/pages/ManageCourses/ManageCourses.tsx
+++ src/pages/ManageCourses/ManageCourses.tsx
@@ -1,102 +1,16 @@
-import schoolLogo from '../../assets/uniLogo.svg'
-import icDashboard from '../../assets/iconDash.svg'
-import icNoti from '../../assets/ic_notifications.svg'
-import icSem from '../../assets/ic_semester.svg'
-import icCourses from '../../assets/ic_courses.svg'
-import icUsers from '../../assets/ic_user.svg'
-import icAudit from '../../assets/ic_audit.svg'
-import icAvatar from '../../assets/profile.svg'
-import icCaretDown from '../../assets/caretdown.svg'
 import icSearch from '../../assets/ic_search.svg'
 import icList from '../../assets/ic_listDown.svg'
 import icSearchPage from '../../assets/ic_seachPageNo.svg'
 import { Link } from 'react-router-dom'
-
+import SideNavBar from '../../components/SideNavBar'
+import Header from '../../components/Header'
 
 export default function ManageCourses(props: any) {
     return (
         <div>
-            {/* Side NavBar */}
-            <div className="absolute w-[320px] h-screen top-0 bg-[#ffffff]">
-                <img 
-                    src={schoolLogo}
-                    className="ml-[68px]"
-                    alt="School Logo"
-                />
+            <SideNavBar />
+            <Header />
 
-                <div className='px-[10px] py-[8px] ml-[16px] mt-[75px]'>
-                    <img 
-                        src={icDashboard}
-                        className='inline-block'
-                        alt='Dashboard Icon'
-                    />
-                    <p className='inline-flex font-[400] text-xl ml-[20px] mt-0 mb-0 text-[#282828]'>Dashboard</p>
-                </div>
-                <div className='px-[10px] py-[8px] ml-[16px]'>
-                    <img 
-                        src={icNoti}
-                        className='inline-block'
-                        alt='Notification Icon'
-                    />
-                    <p className='inline-flex font-[400] text-xl ml-[20px] mt-0 mb-0 text-[#282828]'>Notification</p>
-                </div>
-
-                <br></br>
-                <p className='font-bold text-2xl ml-[30px]'>Management</p>
-
-                <div className='px-[10px] py-[8px] ml-[16px] mt-[20px]'>
-                    <img 
-                        src={icSem}
-                        className='inline-block'
-                        alt='Dashboard Icon'
-                    />
-                    <p className='inline-flex font-[400] text-xl ml-[20px] mt-0 mb-0 text-[#282828]'>Semester</p>
-                </div>
-
-                <div className='bg-[#9DBAFF]'>
-                    <div className='px-[10px] py-[8px] ml-[16px]'>
-                        <img 
-                            src={icCourses}
-                            className='inline-block'
-                            alt='Dashboard Icon'
-                        />
-                        <p className='inline-flex font-[400] text-xl ml-[20px] mt-0 mb-0 text-[#0030A5]'>Courses</p>
-                    </div>
-                </div>
-                
-                <div className='px-[10px] py-[8px] ml-[16px]'>
-                    <img 
-                        src={icUsers}
-                        className='inline-block'
-                        alt='Dashboard Icon'
-                    />
-                    <p className='inline-flex font-[400] text-xl ml-[20px] mt-0 mb-0 text-[#282828]'>Users</p>
-                </div>
-
-                <br></br>
-                <p className='font-bold text-2xl ml-[30px]'>Moderation</p>
-
-                <div className='px-[10px] py-[8px] ml-[16px] mt-[20px]'>
-                    <img 
-                        src={icAudit}
-                        className='inline-block'
-                        alt='Dashboard Icon'
-                    />
-                    <p className='inline-flex font-[400] text-xl ml-[20px] mt-0 mb-0 text-[#282828]'>Audit Log</p>
-                </div>
-            </div>
-
-            {/* Site Header */}
-            <div className='absolute w-[1250px] h-[82px] top-[0px] left-[322px] bg-[#ffffff]'>
-                <h2 className='absolute font-bold text-3xl text-[#282828] ml-[30px] mt-[20px]'>Courses</h2>
-            </div>
-
-            <div className='absolute w-[345px] h-[82px] top-[0px] left-[1573px] bg-[#ffffff]'>
-                <img src={icAvatar} className='absolute ml-[30px] mt-[20px]' alt='Avatar'/>
-                <h3 className='absolute font-medium text-2xl text-[#2B4EA2] pl-[85px] pt-[27px]'>Tom D. Smith</h3>
-                <img src={icCaretDown} className='absolute ml-[260px] mt-[35px]' alt='Caret'/>
-            </div>
-            
             {/* Semester Table */}
             <div className='absolute bg-[#ffffff] w-[1500px] h-[750px] left-[375px] top-[130px] rounded-[10px] overflow-auto'>
                 {/* Table Header */}
@@ -207,11 +121,11 @@ export default function ManageCourses(props: any) {
 
                     <div className='flex'>
                         <button className='h-[30px] w-[30px]'>1</button>
-                        <button className='h-[30px] w-[30px]'>1</button>
-                        <button className='h-[30px] w-[30px]'>1</button>
-                        <button className='h-[30px] w-[30px]'>1</button>
-                        <button className='h-[30px] w-[30px]'>1</button>
-                        <button className='h-[30px] w-[30px]'>1</button>
+                        <button className='h-[30px] w-[30px]'>2</button>
+                        <button className='h-[30px] w-[30px]'>3</button>
+                        <button className='h-[30px] w-[30px]'>8</button>
+                        <button className='h-[30px] w-[30px]'>9</button>
+                        <button className='h-[30px] w-[30px]'>10</button>
                     </div>
 
                     <div className='flex'>
